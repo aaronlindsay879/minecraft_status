254363ea0d8b52825ba9dc8ff4d7a8a4d4fdb1d9
Aaron Lindsay <54952985+aaronlindsay879@users.noreply.github.com>
2023-10-04 14:07:34 +0100

    Feature windowsdns (#2)
    * refactored to make adding platform-specific code nicer
    
    * fixed windows code so it compiles
    
    * fleshed out find_servers for windows
    
    * fixed incorrect crate name

---
+++ dns/src/servers/mod.rs
@@ -0,0 +1,41 @@
+use cfg_if::cfg_if;
+use log::info;
+use std::net::IpAddr;
+
+cfg_if! {
+    if #[cfg(unix)] {
+        mod unix;
+        use unix::find_servers;
+    } else if #[cfg(windows)] {
+        mod windows;
+        use self::windows::find_servers;
+    } else {
+        fn find_servers() -> Option<Vec<IpAddr>> {
+            info!("no supported method for getting dns servers on this platform");
+            None
+        }
+    }
+}
+
+/// Finds DNS servers to use, defaulting to cloudflare if can't find servers client is using
+pub fn dns_servers() -> Vec<IpAddr> {
+    find_servers().unwrap_or_else(|| {
+        let default_servers = vec!["1.1.1.1".parse().unwrap(), "1.0.0.1".parse().unwrap()];
+
+        info!("using default DNS servers: {default_servers:?}");
+        default_servers
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_servers() {
+        let servers = find_servers();
+
+        dbg!(&servers);
+        assert!(servers.is_some());
+    }
+}
---
+++ dns/src/servers/unix.rs
@@ -0,0 +1,23 @@
+use log::info;
+use std::net::IpAddr;
+use std::str::FromStr;
+
+/// Finds dns server in use by parsing /etc/resolv.conf
+pub(crate) fn find_servers() -> Option<Vec<IpAddr>> {
+    let resolv_conf = std::fs::read_to_string("/etc/resolv.conf").ok()?;
+
+    // parse resolv.conf, taking any lines that start with "nameserver " and attempting to parse the ip
+    let servers: Vec<_> = resolv_conf
+        .lines()
+        .filter_map(|line| line.strip_prefix("nameserver "))
+        .filter_map(|line| IpAddr::from_str(line).ok())
+        .collect();
+
+    if servers.len() > 0 {
+        info!("using nameservers from /etc/resolv.conf");
+        Some(servers)
+    } else {
+        info!("no valid nameservers in /etc/resolv.conf");
+        None
+    }
+}
